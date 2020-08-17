ee496041adcfda50d83f4d4bb0e6883798d2547e
hajar98 <hajar.elyemlahi@ensem.ac.ma>
2020-08-17 22:51:44 +0200

    empty cart

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,6 +8,10 @@ import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PersonIcon from '@material-ui/icons/Person';
 import logo from '../assets/logo.png';
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import { Drawer,Button } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -64,8 +68,7 @@ const useStyles = makeStyles(() =>
       lineHeight: 1,
       boxSizing: 'border-box',
       transition: 'all 0.3s ease',
-      outline: 'none !important',
-      background: 'transparent',
+
       fontFamily: 'DIN Next,sans-serif',
       fontWeight: 700,
       fontStyle: 'normal',
@@ -109,11 +112,113 @@ const useStyles = makeStyles(() =>
     flex2: {
       flexGrow: 3,
     },
+  
+  drawer_header:{
+    width:'400px',
+    height:'578px',
+ 
+  },
+  title2:{
+    padding:'0px 50px 0px 20px',
+    width:'370px',
+    height:'80px',
+    display:'flex',
+    alignItems:'center'
+    
+  },
+  title3:{
+    width:'350px',
+    
+  },
+  drawer_title:{
+       verticalAlign: 'middle',
+       fontfamily: 'DIN Next,sans-serif',
+    fontWeight: 900,
+    fontStyle: 'normal',
+    textTransform: 'initial',
+    letterSpacing: '0px',
+    fontSize: '27px',
+    lineHeight: 1,
+    textDecoration: 'none',
+    color: '#202020',
+    webkitFontSmoothing: 'antialiased',
+    textRendering: 'optimizeLegibility',
+    boxSizing: 'border-box',
+    transition: 'all 0.3s ease',
+  },
+  inner_container:{
+    width:'410px',
+    height:'438px',
+    padding:'30px',
+    display: 'flex',
+    flexDirection:'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'#f7f7f7'
+  },
+  inner_container1:{
+    width:'360px',
+    height:'100px',
+    display: 'flex',
+    flexDirection:'column',
+  justifyContent: 'center',
+  alignItems: 'center' ,
+  bottom:0
+  },
+  inner_container2:{
+    fontfamily: 'DIN Next,sans-serif',
+    fontWeight: 400,
+    fonStyle: 'normal',
+    fontSize: '17px',
+    lineHeight: 1.5,
+    color: '#202020'
+  },
+  button: {
+    backgroundColor: '#5600e3',
+    borderColor: '#5600e3',
+    color: '#fff',
+    transition: 'all 0.3s ease',
+    fontFamily: '"DIN Next",sans-serif',
+    fontWeight: 700,
+    fontStyle: 'normal',
+    textTransform: 'initial',
+    letterSpacing: '0.5px',
+    border: '1px solid #f2f2f2',
+    borderRadius: '3px',
+    padding: '16px 24px',
+    lineHeight: 1.5,
+    display: 'inline-block',
+    textAlign: 'center',
+    fontSize: '17px',
+    marginTop: '0px',
+    marginBottom: '60px',
+    '&:hover': {
+      backgroundColor: '#5600e3',
+      opacity: 0.6,
+    },
+  },
+  boxButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+  },
+  close_button:{
+    color: '#202020',  
+  }
   })
 );
 
 function NavBar() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <div className={classes.topHeader}>
@@ -162,9 +267,10 @@ function NavBar() {
                 </IconButton>
                 <IconButton
                   edge='end'
-                  aria-label='account of current user'
+                  aria-label='sopping cart'
                   aria-haspopup='true'
                   color='inherit'
+                  onClick={handleDrawerOpen}
                 >
                   <Badge badgeContent={2} color='secondary'>
                     <ShoppingCartIcon />
@@ -175,6 +281,34 @@ function NavBar() {
           </div>
         </div>
       </AppBar>
+     <div> 
+      <Drawer   variant="persistent" anchor="right" open={open}>
+        <div className={classes.drawer_header}>
+        <div className={classes.title2} >
+          <div className={classes.title3}>
+        <a className={classes.drawer_title}  href='/'>
+          
+         <span className={classes.drawer_title} ><ShoppingCartIcon fontSize='large'/>
+           </span>
+           Your Cart      </a></div>
+          <IconButton  onClick={handleDrawerClose}>
+          <CloseIcon className={classes.close_button} fontSize='small'/> 
+          </IconButton>
+          </div>
+          
+        <Divider />
+        <div className={classes.inner_container} >
+          <div className={classes.inner_container1}>
+          <p className={classes.inner_container2}> Your cart is currently empty. </p>
+          
+          <div className={classes.boxButton}>
+        <Button className={classes.button}>Continue browzing</Button>
+      </div>
+      </div>
+        </div>
+        </div>  
+      </Drawer>
+      </div> 
     </>
   );
 }
---
+++ src/components/emptyCart.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+
+function emptyCart(){
+return(
+    <>
+    
+    
+    
+    
+    
+    
+    </>
+
+);
+
+}
+export default emptyCart
